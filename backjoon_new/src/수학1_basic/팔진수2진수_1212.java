678fecad26c5f494659612cffc822b3f7def1c98
myunggi_moon <myunggi_moon@naver.com>
2022-07-31 16:32:00 +0900

    bfs 숨바꼭질

---
+++ backjoon_new/src/수학1_basic/팔진수2진수_1212.java
@@ -0,0 +1,22 @@
+package 수학1_basic;
+import java.util.Scanner;
+
+public class 팔진수2진수_1212 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int sum=0;
+		int len=s.length();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			String a = Integer.toBinaryString(s.charAt(i)-'0');
+			if(a.length()==2 && i!=0) a="0"+a;
+			else if(a.length()==1 && i!=0) a="00"+a;
+			
+			sb.append(a);
+		}
+		
+		
+		System.out.println(sb);
+	}
+}
