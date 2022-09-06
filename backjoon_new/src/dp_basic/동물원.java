8af7404674d4f40e046dd1ee32960e378bafb342
myunggi_moon <myunggi_moon@naver.com>
2022-09-06 21:35:30 +0900

    동물원 풀이

--- backjoon_new/src/dp_basic/동물원.java
+++ backjoon_new/src/dp_basic/동물원.java
@@ -1,9 +1,26 @@
 package dp_basic;
 
-public class 동물원 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
+public class 동물원 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+	// dp[1][0]=1 , dp[1][1]=1, dp[1][2]=1
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		
+		 int n = Integer.parseInt(br.readLine());
+		 final int MOD =9901;
+		 long dp[][] =new long[n+1][3];
+		 dp[1][0]=dp[1][1]=dp[1][2]=1;
+		 for (int i = 2; i < dp.length; i++) {
+			 dp[i][0]=(dp[i-1][0]+dp[i-1][1]+dp[i-1][2]) % MOD ;
+			 dp[i][1]=(dp[i-1][0]+dp[i-1][2]) % MOD;
+			 dp[i][2]=(dp[i-1][0]+dp[i-1][1]) % MOD;
+		}
+		long ans = (dp[n][0]+dp[n][1]+dp[n][2]) % MOD;
+		System.out.println(ans);
 	}
 
 }
